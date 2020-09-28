8e3ae5c23598a1e9d1b3d1d87cdab6bc59c48a26
PoetOfGod <72036586+PoetOfGod@users.noreply.github.com>
2020-09-28 13:17:34 -0400

    Update InterceptionUA.js

--- Version 12.999/InterceptionUA.js
+++ Version 12.999/InterceptionUA.js
@@ -12,6 +12,6 @@ SourceList["UA:CF"] = {
 AddFightingStyle(["fighter", "ranger", "paladin"], "Interception", {
 	name : "Interception Fighting Style",
 	source : ["UA:CF", 12],
-	description : "\n   " + "When a creature I can see hits a target that is within 5 feet of me with an attack," + "\n   " + "I can use my reaction to reduce the damage the target takes by 1d10 + my" + "\n   " + "proficiency bonus (to a minimum of 0 damage). I must be wielding a shield or a" + "\n   " + "simple martial weapon to use this reaction.",
+	description : "\n   " + "As a reaction, I can reduce the damage dealt to a target I can see within 5 ft of me" + "\n   " + "The damage is reduced by 1d10 + my proficiency bonus (to a minimum of 0 damage)" + "\n   " + "I must be wielding a shield or a simple or martial weapon to use this reaction",
 	action : ["reaction", ""]
 });
